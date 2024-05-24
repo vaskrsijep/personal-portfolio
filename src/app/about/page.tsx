b4e5a8e66719b3424b90e8bc0499026a8d56b0f1
kikij <panicvaskrsije@gmail.com>
2024-05-24 02:08:20 +0200

    adding skills

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,13 +1,39 @@
 "use client"
 import { useState } from "react";
-
+import {delay, motion} from 'framer-motion';
 export default function About() {
+
+    const containerVariants = {
+        hidden: {
+            opacity: 1
+        },
+        visible: {
+            opacity: 1,
+            transition: {
+                staggerChildren: 2,
+                delayChildren: 4
+            },
+        }
+    }
+
+    const itemVariants = {
+        hidden: {
+            opacity: 0,
+            x: -100
+        },
+        visible: {
+            opacity: 1,
+            x: 0,
+        }
+    }
+
     const [hovered, setHovered] = useState(false);
     const [hoveredSkills, setHoveredSkills] = useState(false);
 
     const backendList = ["NodeJS", "ExpressJS", "Django", "FastAPI", "GraphQL", "PostgreSQL", "MongoDB", "Python", "Flask", "PHP", "Java", "ArangoDB", ];
-
     const frontendList = ["ReactJS", "NextJS", "Tailwind CSS", "Bootstrap", "Material UI", "Chakra UI", "VueJS", "NuxtJS", "ViteJS", ];
+    const softSkills = ["Problem Solving", "Technical documentation", "Slack", "Agile", "Teamwork", "Time management", "Task management", "Testing and validation", "Code review", "Astah", "Jira", "Photoshop", "Figma", "Adobe XD", "After Effects", "Vegas Pro", "Cinema 4D", "Git" ];
+    const inProgressLists = ["Improving English Proficiency", "Enhancing UX/UI Design Skills", "Blockchain technology fundamentals" ];
 
     return (
         <div>
@@ -66,31 +92,65 @@ export default function About() {
                         <h2 className="text-4xl uppercase">
                             Frontend
                         </h2>
-                        <div className="flex items-center justify-between gap-3 font-thin text-3xl md:w-9/12 flex-wrap">
+                        <motion.div initial="hidden" whileInView={"visible"} variants={containerVariants} className="flex items-center justify-start gap-3 font-thin text-3xl md:w-9/12 flex-wrap">
                             {
                                 frontendList.map((item, index) => (
-                                    <p className="px-10 py-2 border text-white/50 border-white/50 rounded-full" key={index}>
+                                    <motion.p variants={itemVariants} initial="hidden" whileInView={"visible"} className="px-10 py-2 border text-white/50 border-white/50 rounded-full" key={index}>
                                         {item}
-                                    </p>
+                                    </motion.p>
                                 ))
                             }
 
-                        </div>
+                        </motion.div>
                     </div>
                     <div className="w-full flex items-center justify-center gap-10 flex-col">
                     <h2 className="text-4xl uppercase">
                             Backend
                         </h2>
-                        <div className="flex items-center justify-between gap-3 font-thin text-3xl md:w-9/12 flex-wrap">
+                        <motion.div initial="hidden" whileInView={"visible"  } variants={containerVariants} className="flex items-center justify-start gap-3 font-thin text-3xl md:w-9/12 flex-wrap">
                             {
                                 backendList.map((item, index) => (
-                                    <p className="px-10 py-2 border text-white/50 border-white/50 rounded-full" key={index}>
+                                    <motion.p variants={itemVariants} initial="hidden" whileInView={"visible"} className="px-10 py-2 border text-white/50 border-white/50 rounded-full" key={index}>
                                         {item}
-                                    </p>
+                                    </motion.p>
                                 ))
                             }
 
-                        </div>
+                        </motion.div>
+                    </div>
+                </div>
+
+
+                <div className="grid md:grid-cols-2 grid-cols-1 gap-10 w-full pt-10">
+                    <div className="w-full flex items-center justify-center gap-10 flex-col">
+                        <h2 className="text-4xl uppercase">
+                            Soft Skills
+                        </h2>
+                        <motion.div initial="hidden" whileInView={"visible"} variants={containerVariants} className="flex items-center justify-start gap-3 font-thin text-3xl md:w-9/12 flex-wrap">
+                            {
+                                softSkills.map((item, index) => (
+                                    <motion.p variants={itemVariants} initial="hidden" whileInView={"visible"} className="px-10 py-2 border text-white/50 border-white/50 rounded-full" key={index}>
+                                        {item}
+                                    </motion.p>
+                                ))
+                            }
+
+                        </motion.div>
+                    </div>
+                    <div className="w-full flex items-center justify-center gap-10 flex-col">
+                    <h2 className="text-4xl uppercase">
+                            In Progress
+                        </h2>
+                        <motion.div initial="hidden" whileInView={"visible"} variants={containerVariants} className="flex items-center justify-start gap-3 font-thin text-3xl md:w-9/12 flex-wrap">
+                            {
+                                inProgressLists.map((item, index) => (
+                                    <motion.p variants={itemVariants} initial="hidden" whileInView={"visible"} className="px-10 py-2 border text-white/50 border-white/50 rounded-full" key={index}>
+                                        {item}
+                                    </motion.p>
+                                ))
+                            }
+
+                        </motion.div>
                     </div>
                 </div>
             </div>
